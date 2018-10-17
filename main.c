cb822568fd2b73e8e06522e06dcd02320f718320
djiang2001 <djiang2@stuy.edu>
2018-10-16 23:19:27 -0400

    Rename, working on insert_front and remove_song

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 #include "tunesll.h"
 
+
 int main(){
+  printf("=======LINKED LIST========\n");
   struct song_node* test;
-  print_song;
-  insert_front(test,"Sandstorm","Darude");
-  print_song;
+  printf("Empty List\n");
+  print_song(test);
+  test = insert_front(test,"Sandstorm","Darude");
+  test = insert_front(test,"Hello","World");
+  test = insert_front(test,"Ignite","Zedd");
+  printf("Testing insert front\n");
+  print_song(test);
 
 
   return 0;
--- tunesll.c
+++ tunesll.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <string.h>
 
 struct song_node{ 
   char name[100];
   char artist[100];
   struct song_node *next;
+  
 };
 
-int cmpsong(struct song_node * s1,struct song_node * s2){
+//Helper//
+int songcmp(struct song_node * s1,struct song_node * s2){
   int cmpname = strcmp(s1->name,s2->name);
   int cmpartist = strcmp(s1->artist,s2->artist);
   return cmpname - cmpartist;
@@ -32,34 +33,51 @@ void print_song(struct song_node *head){
   }
 }
 
-struct song_node* insert_front(struct song_node * head,char*n,char*a){
+struct song_node* insert_front(struct song_node *head,char *name,char *artist){
   struct song_node *new_node = malloc(sizeof(struct song_node));
-  strcpy(new_node->name,n);
-  strcpy(new_node->artist,a);
+  strcpy(new_node->name,name);
+  strcpy(new_node->artist,artist);
   new_node->next = head;
   return new_node;
 }
 
-struct song_node* find_song(struct song_node*head, char *songname,char *artistname){
+struct song_node *insert_order(struct song_node *head,char *name,char*artist){
+  struct song_node *new_node = mallc(sizeof(struct song_node));
+  strcpy(new_node->name,name);
+  strcpy(new_node->artist,artist);
+  
+  if(songcmp(head,new_node)>0){
+    new_node->next = head;
+    return new_node;
+  }
+
+  struct song_node *curr = head;
+  while(curr){
+
+
+  }
+
+}
+
+struct song_node* find_song(struct song_node*head, char *name,char *name){
   struct song_node *find;
-  strcpy(find->artist,artistname);
-  strcpy(find->name,songname);
-  while(cmpsong(head,find)){
+  strcpy(find->artist,name);
+  strcpy(find->name,name);
+  while(songcmp(head,find)){
     head = head->next;
   }
   return head;
 }
 
-struct song_node* find_artist(struct song_node*head,char *artistname){
-  while(strcmp(head->artist,artistname)){
+struct song_node* find_artist(struct song_node*head,char *artist){
+  while(strcmp(head->artist,artist)){
     head=head->next;
   }
   return head;
 }
 
 struct song_node* rand_song(struct song_node*head){
-  srand(time(NULL));
-  int r = rand()*songsize(head);
+  int r = rand()%songsize(head);
   while(r){
     head = head->next;
     r--;
@@ -67,14 +85,17 @@ struct song_node* rand_song(struct song_node*head){
   return head;
 }
 
-struct song_node* remove_song(struct song_node* head,char*songname,char*artistname){
+struct song_node* remove_song(struct song_node* head,char*name,char*artist){
   struct song_node* find;
-  strcpy(find->name,songname);
-  strcpy(find->artist,artistname);
-  while(cmpsong(head,find)){
-
-
+  strcpy(find->name,name);
+  strcpy(find->artist,artist);
+  struct song_node *tmp;
+  if(!songcomp(head,find)){
+    tmp = head->next;
+    free(head);
+    return tmp;
   }
+
   
 }
 
